b871d58250078337a2ff929ecb89f3b7855e0633
Anuja Awchar <anujaawchar@gmail.com>
2026-02-28 21:37:02 +0530

    Use dark background for ElectionList page

--- aptcs_frontend/src/pages/ElectionList.jsx
+++ aptcs_frontend/src/pages/ElectionList.jsx
@@ -68,7 +68,7 @@ const ElectionList = () => {
     };
 
     return (
-        <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-screen bg-slate-900/80 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto">
                 <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-8">
                     <div>
