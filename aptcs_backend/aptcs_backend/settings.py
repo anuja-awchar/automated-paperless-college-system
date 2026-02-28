872e27e6cea0139a4925b95cea64f6cc40804351
Anuja Awchar <anujaawchar@gmail.com>
2026-02-28 21:19:08 +0530

    Set DEFAULT_AUTO_FIELD to BigAutoField

--- aptcs_backend/aptcs_backend/settings.py
+++ aptcs_backend/aptcs_backend/settings.py
@@ -133,6 +133,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 # Custom User Model
 AUTH_USER_MODEL = 'users.User'
 
